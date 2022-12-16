23ae71317de88b6a8a4cd5d7e5aa58bf977d4d38
hamdyadam97 <hamdyadam97@gmail.com>
2022-12-17 00:05:23 +0200

    dast

--- project/settings.py
+++ project/settings.py
@@ -90,22 +90,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'veterinarypharmacy',
-       'USER': 'postgres',
-       'PASSWORD': '123',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
-}
 # DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'veterinarypharmacy',
+#        'USER': 'postgres',
+#        'PASSWORD': '123',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
+DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-# }
+}
 
 AUTH_USER_MODEL = 'user.User'
 
